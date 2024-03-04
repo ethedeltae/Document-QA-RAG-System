31a14b341b236d639329faeef7e109424166b937
Abhilekh Borah <114558002+ethedeltae@users.noreply.github.com>
2024-03-05 01:04:29 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from llama_index.core import SimpleDirectoryReader
+
+from llama_index.core import VectorStoreIndex
+
+from llama_index.llms.gemini import Gemini
+
+from IPython.display import Markdown, display
+
+from llama_index.core import ServiceContext
+
+from llama_index.core import StorageContext, load_index_from_storage
+
+import google.generativeai as genai
+
+from llama_index.embeddings.gemini import GeminiEmbedding
+
+
+
+
